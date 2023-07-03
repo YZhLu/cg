ddac6b1ed05fa5af8276e2bba08f9d2d135b511a
YZhLu <luiz.zito@icloud.com>
2023-07-02 21:20:05 -0300

    muchu loku, add colisoes, but ta bem +-. falta add colisao dos flipers e condicao de parada

--- src/gl_models/ball_model.py
+++ src/gl_models/ball_model.py
@@ -1,10 +1,11 @@
 from gl_models import sphere_model
 from OpenGL import GL as gl
+from ball_position import BallPosition
 
-def draw(size: list = [1,1,1], pos: list = [0,0,0], color: list = [1.0, 1.0, 1.0], texture_id: int = 0, rangle: float = 0, raxis: list = [0, 0, 0]):
+def draw(ball_pos: BallPosition, size: list = [1,1,1], color: list = [1.0, 1.0, 1.0], texture_id: int = 0, rangle: float = 0, raxis: list = [0, 0, 0]):
     gl.glPushMatrix()
     #  origem posicionada no centro da peça
-    gl.glTranslatef(pos[0], pos[1], pos[2])
+    gl.glTranslatef(ball_pos.sphere_position[0], ball_pos.sphere_position[1], ball_pos.sphere_position[2])
     #  os angulos e eixos de rotação
     gl.glRotatef(rangle, raxis[0], raxis[1], raxis[2])
     # size
